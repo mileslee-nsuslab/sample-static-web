241ba104bb670ac23eabff904b4913d7e1243c53
Admin <admin@Admins-MacBook-Pro.local>
2023-08-02 03:23:46 +0900

    first commit

---
+++ src/config.js
@@ -0,0 +1,14 @@
+const config = {
+  blogName: "Blog",
+  authorName: "Kartik Nair",
+  authorDescription:
+    "a web developer and design enthusiast who's building stuff in Dubai",
+  authorWebsite: "https://kartikn.me",
+  blogDescription: "Sharing what I learn as a web developer & designer",
+  dev: {
+    postsdir: "./content",
+    outdir: "./public"
+  }
+};
+
+module.exports = config;
