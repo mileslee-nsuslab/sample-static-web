e53fab2028b8430bcbe04d778fcf4517fb4bdd4d
Admin <admin@Admins-MacBook-Pro.local>
2023-08-02 14:27:26 +0900

    update authorname

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
   blogName: "Blog",
-  authorName: "Miles.lee",
+  authorName: "Miles.lee 2",
   authorDescription:
     "a web developer and design enthusiast who's building stuff in Dubai",
   authorWebsite: "https://kartikn.me",
